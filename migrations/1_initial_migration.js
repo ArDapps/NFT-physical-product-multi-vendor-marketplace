7523d22eabb34c4e36ae63010ca1442d22e287dd
دابس بالعربي <arabicdapps@gmail.com>
2023-10-07 17:22:42 +0300

    add semi work with smart contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
-const Migrations = artifacts.require("Migrations");
+const Token = artifacts.require("Token");
+const NFTMarketPlace = artifacts.require("NFTMarketPlace");
+const NFT = artifacts.require("NFT");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(NFTMarketPlace);
+  const market = await NFTMarketPlace.deployed();
+  await deployer.deploy(NFT, market.address);
 };
